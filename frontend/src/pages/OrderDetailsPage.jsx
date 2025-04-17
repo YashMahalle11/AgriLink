446cffb93b61751450cc883031ce6b9aaec2c7db
YashMahalle11 <yashmahalle2003@gmail.com>
2025-04-17 18:38:16 +0530

    Fix product link by accessing productId._id

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -95,7 +95,7 @@ const OrderDetailsPage = () => {
                      alt={item.name}
                     className='w-12 h-12 object-cover rounded-lg mr-4'
                    />
-                   <Link to={`/product/${item.productId}`} className='text-blue-500 hover:underline'>
+                   <Link to={`/product/${item.productId._id}`} className='text-blue-500 hover:underline'>
                        {item.name}
                    </Link>
                    </td>
